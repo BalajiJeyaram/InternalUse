98aacdd3bfcf038608166176f7c93c9d6bb27836
BalajiKuduva <kjbalaji@gmail.com>
2019-12-18 16:20:42 -0500

    XamlSample Base Version
    This is Android sample app created using Xamarin.

---
+++ XamlSamples/XamlSamples/XamlSamples/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿
+using Xamarin.Forms;
+
+namespace XamlSamples
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            Button button = new Button
+            {
+                Text = "Navigate!",
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.Center
+            };
+
+            button.Clicked += async (sender, args) =>
+            {
+                //await Navigation.PushAsync(new HelloXamlPage());
+                await Navigation.PushAsync(new XamlPlusCodePage());
+            };
+
+            Content = button;
+        }
+    }
+}
---
+++ XamlSamples/XamlSamples/XamlSamples/XamlPlusCodePage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamlSamples
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class XamlPlusCodePage : ContentPage
+	{
+        //private Label valueLabel;
+
+        //private void InitialComponenent()
+        //{
+        //    this.LoadFromXaml(typeof(XamlPlusCodePage));
+        //    valueLabel = this.FindByName<Label>("valueLabel");
+        //}
+		public XamlPlusCodePage ()
+		{
+			InitializeComponent ();
+		}
+
+        private void Slider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            
+        }
+
+        void OnSliderValueChanged(object sender, ValueChangedEventArgs args)
+        {
+            //valueLabel.Text = e.NewValue.ToString("F3");
+            valueLabel.Text = ((Slider)sender).Value.ToString("F3");
+        }
+
+        async void OnButtonClicked(Object sender, EventArgs args)
+        {
+            Button button = (Button)sender;
+            await DisplayAlert("Clicked!",
+                "The button labeled '" + button.Text +" ' has been changed",
+                "OK");
+
+        }
+    }
+}
