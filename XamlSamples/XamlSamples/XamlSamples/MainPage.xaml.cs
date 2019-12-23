0469d9fb7faac6cd816d025c7b99ea150d330193
BalajiKuduva <kjbalaji@gmail.com>
2019-12-23 15:00:38 -0500

    Update MainPage.xaml.cs

--- XamlSamples/XamlSamples/XamlSamples/MainPage.xaml.cs
+++ XamlSamples/XamlSamples/XamlSamples/MainPage.xaml.cs
@@ -7,21 +7,56 @@ namespace XamlSamples
     {
         public MainPage()
         {
+
             InitializeComponent();
-            Button button = new Button
-            {
-                Text = "Navigate!",
-                HorizontalOptions = LayoutOptions.Center,
-                VerticalOptions = LayoutOptions.Center
+
+            Button button = new Button {
+                Text = "Navigate!", HorizontalOptions = LayoutOptions.Center, VerticalOptions = LayoutOptions.Center, WidthRequest = 150   
             };
 
-            button.Clicked += async (sender, args) =>
-            {
-                //await Navigation.PushAsync(new HelloXamlPage());
+            button.Clicked += async (sender, args) => {
                 await Navigation.PushAsync(new XamlPlusCodePage());
             };
 
-            Content = button;
+            Button btncalculator = new Button {
+                Text = "Calculator", HorizontalOptions = LayoutOptions.Center, VerticalOptions = LayoutOptions.Center, WidthRequest = 150
+            };
+
+            btncalculator.Clicked += async (sender, args) => {
+                await DisplayAlert("Alert","Coming Shortly!!!","OK","CANCEL");
+            };
+
+            Button btnTime = new Button {
+                Text = "Time", HorizontalOptions = LayoutOptions.Center, VerticalOptions = LayoutOptions.Center, WidthRequest = 150
+            };
+
+            btnTime.Clicked += async (sender, args) => {
+                await DisplayAlert("Time", LocalDate(true), "OK", "CANCEL");
+            };
+
+            Button btnDate = new Button {
+                Text = "Date",HorizontalOptions = LayoutOptions.Center,VerticalOptions = LayoutOptions.Center,WidthRequest = 150
+            };
+
+            btnDate.Clicked += async (sender, args) => {
+                await DisplayAlert("Time", LocalDate(false), "OK", "CANCEL");
+            };
+
+            StackLayout stack = new StackLayout { Children = { button, btncalculator, btnTime, btnDate } };
+            Content = stack;
+
         }
+
+        private string LocalDate(bool time = false)
+        {
+            string result = string.Empty;
+            result = System.DateTime.Now.ToString("g");
+            if (time)
+                result = System.DateTime.Now.ToString("hh:mm:ss");
+
+            return result;
+        }
+
+
     }
 }
