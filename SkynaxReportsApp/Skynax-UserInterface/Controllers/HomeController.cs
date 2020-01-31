3816af47259b17f7f21de09557d0043911ed40e8
BalajiKuduva <kjbalaji@gmail.com>
2020-01-31 14:46:21 -0500

    Assessmentpage

--- SkynaxReportsApp/Skynax-UserInterface/Controllers/HomeController.cs
+++ SkynaxReportsApp/Skynax-UserInterface/Controllers/HomeController.cs
@@ -3,11 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Skynax_UserInterface.Models;
 
 namespace Skynax_UserInterface.Controllers
 {
+    
     public class HomeController : Controller
     {
+        private List<QuestionAnswer> QAT = new List<QuestionAnswer>();
+        public HomeController()
+        {
+            QAT.Add(new QuestionAnswer() { Answer="Sample",AnswerType=1,Question="What's your name"});
+            QAT.Add(new QuestionAnswer() { Answer = "1234", AnswerType = 1, Question = "What's your mother name" });
+            QAT.Add(new QuestionAnswer() { Answer = "Sample", AnswerType = 1, Question = "What's your name" });
+            QAT.Add(new QuestionAnswer() { Answer = "1234", AnswerType = 1, Question = "What's your mother name" });
+            QAT.Add(new QuestionAnswer() { Answer = "Sample", AnswerType = 1, Question = "What's your name" });
+            QAT.Add(new QuestionAnswer() { Answer = "1234", AnswerType = 1, Question = "What's your mother name" });
+        }
+        
         public ActionResult Index()
         {
             return View();
@@ -25,6 +38,19 @@ namespace Skynax_UserInterface.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        public ActionResult Assessment()
+        {
+            ViewBag.Message = "Assessment Page";
+
+            return View(QAT);
+        }
+        public JsonResult GetQuestions()
+        {
+            ViewBag.Message = "GetQuestions Method";
+
+            return Json(QAT, JsonRequestBehavior.AllowGet);
         }
     }
 }
---
+++ SkynaxReportsApp/Skynax-UserInterface/Models/QuestionAnswer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Skynax_UserInterface.Models
+{
+    public class QuestionAnswer
+    {
+        public string Question { get; set; }
+        public string Answer { get; set; }
+        public int AnswerType { get; set; }
+    }
+}
