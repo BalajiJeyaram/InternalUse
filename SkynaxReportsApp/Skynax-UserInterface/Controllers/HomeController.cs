ab7a8cd554164da611c323b5d483b9114c2878be
BalajiKuduva <kjbalaji@gmail.com>
2020-02-04 14:42:18 -0500

    Assessment code changes

--- SkynaxReportsApp/Skynax-UserInterface/Controllers/HomeController.cs
+++ SkynaxReportsApp/Skynax-UserInterface/Controllers/HomeController.cs
@@ -13,12 +13,12 @@ namespace Skynax_UserInterface.Controllers
         private List<QuestionAnswer> QAT = new List<QuestionAnswer>();
         public HomeController()
         {
-            QAT.Add(new QuestionAnswer() { Answer="Sample",AnswerType=1,Question="What's your name"});
-            QAT.Add(new QuestionAnswer() { Answer = "1234", AnswerType = 1, Question = "What's your mother name" });
-            QAT.Add(new QuestionAnswer() { Answer = "Sample", AnswerType = 1, Question = "What's your name" });
-            QAT.Add(new QuestionAnswer() { Answer = "1234", AnswerType = 1, Question = "What's your mother name" });
-            QAT.Add(new QuestionAnswer() { Answer = "Sample", AnswerType = 1, Question = "What's your name" });
-            QAT.Add(new QuestionAnswer() { Answer = "1234", AnswerType = 1, Question = "What's your mother name" });
+            QAT.Add(new QuestionAnswer() { Answer="It is Honeywell Device#It is Operating System#It is Toothpaste",AnswerType=1,Question="What is EDA51"});
+            QAT.Add(new QuestionAnswer() { Answer = "It is a Honeywell Application#It is an Apple Product#It is an Amazon app", AnswerType = 1, Question = "What is Honeywell Provisioner" });
+            QAT.Add(new QuestionAnswer() { Answer = "Description...", AnswerType = 2, Question = "Write 2-3 lines about SMART TE" });
+            QAT.Add(new QuestionAnswer() { Answer = "It is Honeywell Device#It is Operating System#It is Toothpaste", AnswerType = 1, Question = "What is Dolphin" });
+            QAT.Add(new QuestionAnswer() { Answer = "It is Enterprise Application#It is a website where an enduser can buy product#It is thirdparty Website", AnswerType = 1, Question = "What is SalesForce" });
+            QAT.Add(new QuestionAnswer() { Answer = "Description...", AnswerType = 2, Question = "Write 2-3 lines about CN60" });
         }
         
         public ActionResult Index()
