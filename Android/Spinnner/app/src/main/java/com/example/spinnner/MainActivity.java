9eeac6fc3b2ed25fce9b1fec05221ab7b5fe40ec
Balaji Kuduva <balaji.kj@honeywell.com>
2021-04-23 17:00:51 -0400

    Modified on Spinner, New activity 1 and 2
    Modified on Spinner, New activity 1 and 2

--- Android/Spinnner/app/src/main/java/com/example/spinnner/MainActivity.java
+++ Android/Spinnner/app/src/main/java/com/example/spinnner/MainActivity.java
@@ -3,10 +3,13 @@ package com.example.spinnner;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +23,20 @@ public class MainActivity extends AppCompatActivity {
 
         myadapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         mySpinner.setAdapter(myadapter);
+
+        mySpinner.setOnItemSelectedListener(this);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String text = parent.getItemAtPosition(position).toString();
+        Toast.makeText(parent.getContext(), text, Toast.LENGTH_SHORT).show();
+
+        //training video link: https://www.youtube.com/watch?v=eL69kj-_Wvs
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
